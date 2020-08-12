ab2ee3e4df338ae412859e7f7a0535062fcf0600
vinaytet75 <vinaytest75@gmail.com>
2020-08-12 18:31:13 +0530

    added charge per day functionality

--- booking.js
+++ booking.js
@@ -1,3 +1,25 @@
+window.onload = ()=>{
+    
+}
+
+
+
+
+
+
+//Function for finding Charge per day
+function dayCharge(startDate ,endDate,dailyCharge){
+    let startT = new Date(startDate)
+    let endT = new Date(endDate)
+
+    let elapsed =   endT.getTime() - startT.getTime()
+    let day = Math.ceil(elapsed/86400000)
+    let charge = day*dailyCharge
+
+    return charge
+}
+
+// Function for Google map with Bangalore Latitude & Longitude
 function myMap() {
     var mapProp= {
         center:new google.maps.LatLng(12.9716,77.5946),
