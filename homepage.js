d4e05ecf761f93ffd333718b685679bb2143d614
Aloknath-sah <kumaraditya6396@gmail.com>
2020-08-13 13:51:58 +0530

    book now button function added

--- homepage.js
+++ homepage.js
@@ -1,7 +1,54 @@
 var data=[]
 
 window.onload = ()=>{
-    
+    var i20 = document.getElementById("i20")
+    i20.addEventListener("click",handlei20=()=>{
+        v_name = localStorage.setItem("veh","MODEL NAME-i20")
+        window.location = "booking.html"
+    })
+
+    var swift = document.getElementById("swift")
+    swift.addEventListener("click",handleswift=()=>{
+        v_name = localStorage.setItem("veh","MODEL NAME-swift")
+        window.location = "booking.html"
+    })
+
+    var eco = document.getElementById("eco")
+    eco.addEventListener("click",handleeco=()=>{
+        v_name = localStorage.setItem("veh","MODEL NAME-Ford ecosport")
+        window.location = "booking.html"
+    })
+
+    var baleno = document.getElementById("baleno")
+    baleno.addEventListener("click",handlebaleno=()=>{
+        v_name = localStorage.setItem("veh","MODEL NAME-Maruti suzuki Baleno")
+        window.location = "booking.html"
+    })
+
+    var nexon = document.getElementById("nexon")
+    nexon.addEventListener("click",handlenexon=()=>{
+        v_name = localStorage.setItem("veh","MODEL NAME-TATA NEXON")
+        window.location = "booking.html"
+    })
+
+    var polo = document.getElementById("polo")
+    polo.addEventListener("click",handlepolo=()=>{
+        v_name = localStorage.setItem("veh","MODEL NAME-Volkswagen polo")
+        window.location = "booking.html"
+    })
+
+    var kuv = document.getElementById("kuv")
+    kuv.addEventListener("click",handlekuv=()=>{
+        v_name = localStorage.setItem("veh","MODEL NAME-Mahindra KUV")
+        window.location = "booking.html"
+    })
+
+    var creta = document.getElementById("creta")
+    creta.addEventListener("click",handlecreta=()=>{
+        v_name = localStorage.setItem("veh","MODEL NAME-Hyundai Creta")
+        window.location = "booking.html"
+    })
+
     var r_form = document.getElementById("register_form")
     r_form.addEventListener("submit", handleRegForm)
 
@@ -13,21 +60,31 @@ const handleRegForm=()=>{
     event.preventDefault()
     var email = document.getElementById("r_email").value
     var password = document.getElementById("r_password").value
-    data.push({email,password})
-    console.log(data)
+    if(data.length == 0 && localStorage.getItem("registration").length == 0){
+        data.push({email,password})
+        var a = JSON.stringify(data)
+        localStorage.setItem("registration",a)
+        console.log(data)
+    }
 }
 
 const handleLogForm=()=>{
     event.preventDefault()
     var l_email = document.getElementById("l_email").value
     var l_password = document.getElementById("l_password").value
-    if(data[0].email == l_email && data[0].password == l_password){
-        data.pop()
-        window.location = "booking.html"
+    var b = localStorage.getItem("registration")
+    var res = JSON.parse(b)
+    console.log(res)
+    if(res[0].email == l_email && res[0].password == l_password){
+        var btn = document.getElementById("login")
+        btn.innerHTML = "logout"
     }
-    else{
-        handleError()
-    }
+    // if(data[0].email == l_email && data[0].password == l_password){
+    //     data.pop()
+    // }
+    // else{
+    //     handleError()
+    // }
 }
 
 const handleError=()=>{
